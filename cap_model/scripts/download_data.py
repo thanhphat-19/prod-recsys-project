4d0ec66cde983340e49abc52bab7fab5aa417a5a
Phat Ngo <thanhphat352@gmail.com>
2025-12-07 19:46:38 +0700

    feat: download data scipts

---
+++ cap_model/scripts/download_data.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import kaggle
+
+def download_dataset():
+    DATASET = "rikdifos/credit-card-approval-prediction"
+    TARGET_DIR = "../cap_model/data/raw"
+
+    print(f"[INFO] Creating target directory: {TARGET_DIR}")
+    os.makedirs(TARGET_DIR, exist_ok=True)
+
+    print(f"[INFO] Downloading dataset: {DATASET}")
+    kaggle.api.dataset_download_files(
+        dataset=DATASET,
+        path=TARGET_DIR,
+        unzip=True
+    )
+
+    print(f"[DONE] Dataset downloaded and extracted to: {TARGET_DIR}")
+
+
+if __name__ == "__main__":
+    download_dataset()
